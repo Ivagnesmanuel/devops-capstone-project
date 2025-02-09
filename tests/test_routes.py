848f6037c9da76aabbe54a8061a729f9b46217aa
ivagnesmanuel <ivagnesmanuel@users.noreply.github.com>
2025-02-09 16:33:33 +0100

    fix for flake8

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -27,7 +27,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 #  T E S T   C A S E S
 ######################################################################
 class TestAccountService(TestCase):
-    """Account Service Tests"""       
+    """Account Service Tests"""
 
     @classmethod
     def setUpClass(cls):
